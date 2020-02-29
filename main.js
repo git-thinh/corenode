c9941d128ce8b788ce275b78c35a02d9be5ca0c6
Mr Thinh <33323583+git-thinh@users.noreply.github.com>
2020-02-29 11:32:55 +0700

    a

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+const HTTP_PORT = 12345;
+const IP = '127.0.0.1';
+
+const LOG_PORT = 1515;
+const LOG_ERROR_PORT = 1510;
 
 const UUID = require('uuid');
 const JOB = require('cron').CronJob;
@@ -10,32 +15,43 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 const _mod_file = require('./lib/_mod_file.js');
 
-
 let ___API = {};
-let ___FN = {};
+let ___JOB = {};
 
-
-_mod_file.get_objects('bot', 'js').then(data => {
-    ___FN = data;
+_mod_file.get_objects('job', 'js').then(data => {
+    ___JOB = data;
 
     for (var api in data) {
         for (var key in data[api]) {
             const js = '(function () { \
                             try{ \
-                                ___FN["' + api + '"]["' + key + '"].execute = ' + data[api][key].text + '; \
-                                ___FN["' + api + '"]["' + key + '"].ok = true; \
+                                ___JOB["' + api + '"]["' + key + '"].execute = ' + data[api][key].text + '; \
+                                ___JOB["' + api + '"]["' + key + '"].ok = true; \
                             }catch(e){ \
-                                ___FN["' + api + '"]["' + key + '"].ok = false; \
+                                ___JOB["' + api + '"]["' + key + '"].ok = false; \
                             } \
                         })()';
             eval(js);
         }
     }
 
-    console.log('????????? = ', ___FN);
+    //console.log('????????? = ', ___JOB);
 });
 
+//#region [ HTTP ]
 
+const HTTP_EXPRESS = require('express');
+const HTTP_BODY_PARSER = require('body-parser');
+const HTTP_APP = HTTP_EXPRESS();
+const HTTP_SERVER = require('http').createServer(HTTP_APP);
 
+HTTP_APP.use(HTTP_EXPRESS.static(PATH.join(__dirname, 'htdoc')));
+
+HTTP_APP.get('/test/job', async (req, res) => { res.json(___JOB); });
+HTTP_APP.get('/test/api', async (req, res) => { res.json(___API); });
+
+HTTP_SERVER.listen(HTTP_PORT, IP);
+
+//#endregion
 
 
